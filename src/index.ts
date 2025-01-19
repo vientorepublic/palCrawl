491baee5657b77e596913f9bdb26267418bceb1f
vientorepublic <markgod0419@gmail.com>
2025-01-19 15:00:56 +0900

    ♻️ refactoring: code cleanup, fix num variable

--- src/index.ts
+++ src/index.ts
@@ -3,39 +3,42 @@ import type { ITableData } from "./types";
 import * as cheerio from "cheerio";
 import { Config } from "./config";
 
-async function getHTML(url: string): Promise<string> {
+async function getPalHTML(): Promise<string> {
   try {
-    const http = axios.create({
+    const res = await axios.get<string>(Config.URL, {
       baseURL: Config.DOMAIN,
-    });
-    const res = await http.get(url, {
       headers: {
         "User-Agent": Config.UserAgent,
       },
     });
-    return res.data as string;
+    return res.data;
   } catch (err) {
     if (isAxiosError(err) && err.response) {
-      throw new Error(err.response.statusText);
+      throw new Error(`Invalid response: ${err.response.status} ${err.response.statusText}`);
     } else {
-      throw new Error();
+      if (err instanceof Error) {
+        throw new Error(err.message);
+      } else {
+        console.error(err);
+        throw new Error("Unknown Error");
+      }
     }
   }
 }
 
-function parseHTML(data: string): ITableData[] {
-  const $ = cheerio.load(data);
+function parseTable(html: string): ITableData[] {
+  const $ = cheerio.load(html);
   const body = $("body");
   const table = body.find("table > tbody > tr");
   const output: ITableData[] = [];
   table.map((i, el) => {
-    let subject = $(el).find("td.td_block > a.board_subject").text().trim();
+    const subject = $(el).find("td.td_block > a.board_subject").text().trim();
     if (subject) {
       const link = $(el).find("td.td_block > a.board_subject").attr("href");
       const numComments = Number($(el).find("td:nth-child(8)").text().replace(",", "").trim());
       const proposerCategory = $(el).find("td:nth-child(3)").text().trim();
       const committee = $(el).find("td:nth-child(4)").text().trim();
-      const num = i + 1;
+      const num = Number($(el).find("td:nth-child(1)").text().trim());
       let boardLink = "";
       if (link) {
         boardLink = Config.DOMAIN + link;
@@ -54,7 +57,7 @@ function parseHTML(data: string): ITableData[] {
 }
 
 async function main() {
-  const html = await getHTML(Config.URL);
-  console.log(parseHTML(html));
+  const html = await getPalHTML();
+  console.log(parseTable(html));
 }
 main();
