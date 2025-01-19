0df55bc1be11e81f64dd20e543cd00ba42ff945a
vientorepublic <markgod0419@gmail.com>
2025-01-19 17:38:57 +0900

    🎨 improve code structure by switch to oop

--- src/index.ts
+++ src/index.ts
@@ -3,61 +3,66 @@ import type { ITableData } from "./types";
 import * as cheerio from "cheerio";
 import { Config } from "./config";
 
-async function getPalHTML(): Promise<string> {
-  try {
-    const res = await axios.get<string>(Config.URL, {
-      baseURL: Config.DOMAIN,
-      headers: {
-        "User-Agent": Config.UserAgent,
-      },
-    });
-    return res.data;
-  } catch (err) {
-    if (isAxiosError(err) && err.response) {
-      throw new Error(`Invalid response: ${err.response.status} ${err.response.statusText}`);
-    } else {
-      if (err instanceof Error) {
-        throw new Error(err.message);
+export class PalCrawl {
+  public async getPalHTML(): Promise<string> {
+    try {
+      const res = await axios.get<string>(Config.URL, {
+        baseURL: Config.DOMAIN,
+        headers: {
+          "User-Agent": Config.UserAgent,
+        },
+      });
+      return res.data;
+    } catch (err) {
+      if (isAxiosError(err) && err.response) {
+        throw new Error(`Invalid response: ${err.response.status} ${err.response.statusText}`);
       } else {
-        console.error(err);
-        throw new Error("Unknown Error");
+        if (err instanceof Error) {
+          throw new Error(err.message);
+        } else {
+          console.error(err);
+          throw new Error("Unknown Error");
+        }
       }
     }
   }
-}
 
-function parseTable(html: string): ITableData[] {
-  const $ = cheerio.load(html);
-  const body = $("body");
-  const table = body.find("table > tbody > tr");
-  const output: ITableData[] = [];
-  table.map((i, el) => {
-    const subject = $(el).find("td.td_block > a.board_subject").text().trim();
-    if (subject) {
-      const link = $(el).find("td.td_block > a.board_subject").attr("href");
-      const numComments = Number($(el).find("td:nth-child(8)").text().replace(",", "").trim());
-      const proposerCategory = $(el).find("td:nth-child(3)").text().trim();
-      const committee = $(el).find("td:nth-child(4)").text().trim();
-      const num = Number($(el).find("td:nth-child(1)").text().trim());
-      let boardLink = "";
-      if (link) {
-        boardLink = Config.DOMAIN + link;
+  public parseTable(html: string): ITableData[] {
+    const $ = cheerio.load(html);
+    const body = $("body");
+    const table = body.find("table > tbody > tr");
+    const output: ITableData[] = [];
+    table.map((i, el) => {
+      const subject = $(el).find("td.td_block > a.board_subject").text().trim();
+      if (subject) {
+        const link = $(el).find("td.td_block > a.board_subject").attr("href");
+        const numComments = Number($(el).find("td:nth-child(8)").text().replace(",", "").trim());
+        const proposerCategory = $(el).find("td:nth-child(3)").text().trim();
+        const committee = $(el).find("td:nth-child(4)").text().trim();
+        const num = Number($(el).find("td:nth-child(1)").text().trim());
+        let boardLink = "";
+        if (link) {
+          boardLink = Config.DOMAIN + link;
+        }
+        output.push({
+          num,
+          subject,
+          proposerCategory,
+          committee,
+          numComments,
+          link: boardLink,
+        });
       }
-      output.push({
-        num,
-        subject,
-        proposerCategory,
-        committee,
-        numComments,
-        link: boardLink,
-      });
-    }
-  });
-  return output;
+    });
+    return output;
+  }
+
+  public async main(): Promise<void> {
+    const html = await this.getPalHTML();
+    const table = this.parseTable(html);
+    console.log(table);
+  }
 }
 
-async function main() {
-  const html = await getPalHTML();
-  console.log(parseTable(html));
-}
-main();
+const palCrawl = new PalCrawl();
+palCrawl.main();
