775adafc532f9fba11584ec72b0de57e467d87f9
vientorepublic <markgod0419@gmail.com>
2025-01-29 14:58:49 +0900

    test: add some tests

--- src/pal.test.ts
+++ src/pal.test.ts
@@ -1,9 +1,19 @@
 import { PalCrawl } from './pal';
 
 describe('PalCrawl', () => {
+  test('getPalHTML: should be return string', async () => {
+    const palCrawl = new PalCrawl();
+    const html = await palCrawl.getPalHTML();
+    expect(typeof html).toBe('string');
+  });
+  test('parseTable: array length should be 0 when invalid html', () => {
+    const palCrawl = new PalCrawl();
+    const table = palCrawl.parseTable('asdf');
+    expect(table).toHaveLength(0);
+  });
   test('get: array length should be 10', async () => {
     const palCrawl = new PalCrawl();
-    const list = await palCrawl.get();
-    expect(list).toHaveLength(10);
+    const table = await palCrawl.get();
+    expect(table).toHaveLength(10);
   });
 });
